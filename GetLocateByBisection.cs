9c67681e5f888f4eb9958ebd15c0bf31e104f273
CodeMover1 <35393335+CodeMover1@users.noreply.github.com>
2018-01-22 15:29:57 +0800

    Add files via upload

---
+++ GetLocateByBisection.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace learn20180109
+{
+    class GetLocateByBisection
+    {
+        public string Locate(int[]XX,int N, int X)
+        {
+            int tem_n;
+            int j1, ju, jm;
+            j1 = 0;
+            ju = N + 1;
+            rebound:
+            if (ju - j1 > 1)
+            {
+                jm = (ju + j1)/2;
+                if (XX[N] > XX[1] == X > XX[jm])
+                    j1 = jm;
+                else
+                {
+                    ju = jm;
+                }
+                goto rebound;
+            }
+            tem_n = j1;
+            if (X - XX[tem_n] > (XX[tem_n + 1] - N))
+                tem_n = j1 + 1;
+            return "a[" + tem_n.ToString() + "]" + XX[tem_n].ToString();
+        }
+    }
+}
